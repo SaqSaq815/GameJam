0288d5285ef71453a402deeb6a152450821f7e1e
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2023-01-15 14:04:00 -0500

    worked on player and repeating bg

--- Assets/Scripts/RepeatingBg.cs
+++ Assets/Scripts/RepeatingBg.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RepeatingBg : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float endX;
+    public float startX;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= endX)
+        {
+            Vector2 pos = new Vector2(startX, transform.position.y);
+            transform.position = pos;
+        }
     }
 }
