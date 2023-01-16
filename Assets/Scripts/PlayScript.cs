ff4b5956308d73240a25017d0957c0e022d8429d
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2023-01-15 20:58:04 -0500

    audio + other fixes

--- Assets/Scripts/PlayScript.cs
+++ Assets/Scripts/PlayScript.cs
@@ -5,10 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject option;
+    public GameObject credit;
+    public GameObject creditCan;
+
     public void Play()
     {
         SceneManager.LoadScene("Game_Scene");
        
+    }
+
+    public void Menu()
+    {
+        menu.SetActive(true);
+        option.SetActive(false);
+        credit.SetActive(false);
+        creditCan.SetActive(false);
+    }
+    public void Options()
+    {
+        menu.SetActive(false);
+        option.SetActive(true);
+        credit.SetActive(false);
+        creditCan.SetActive(false);
+    }
+    public void Credit()
+    {
+        menu.SetActive(false);
+        option.SetActive(false);
+        credit.SetActive(true);
+        creditCan.SetActive(true);
     }
 
     public void Home()
